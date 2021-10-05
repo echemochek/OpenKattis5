f5df313a9b050d12cb9b08b9bf0e4e691655b4ac
echemochek <echemochek@gmail.com>
2021-10-05 16:35:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OpenKattis5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("Please enter N, W and H: ");
+
+			string[] inputString = Console.ReadLine().Split(' ');
+
+			int N = Int32.Parse(inputString[0]);
+			int W = Int32.Parse(inputString[1]);
+			int H = Int32.Parse(inputString[2]);
+			double D = Math.Sqrt(W * W + H * H);
+
+			for (int i = 0; i < N; i++)
+			{
+				Console.Write("Please enter length of match: ");
+				int match = Int32.Parse(Console.ReadLine());
+				if (match <= D)
+				{
+					Console.WriteLine("DA");
+				}
+				else
+				{
+					Console.WriteLine("NE");
+				}
+			}
+		}
+    }
+}
